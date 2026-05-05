828bdfe20823f67009609be874c003d42ebb43fd
Samuel <ktnpop001@myuct.ac.za>
2026-05-05 10:51:06 +0200

    Use standard Frappe setup.py format reading from requirements.txt

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("requirements.txt") as f:
+    install_requires = [line for line in f.read().strip().split("\n") if line and not line.startswith("#")]
+
 setup(
     name="brownhat",
     version="0.1.1",
@@ -9,5 +12,5 @@ setup(
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=[],
+    install_requires=install_requires,
 )
